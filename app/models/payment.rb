4f1934ce2b2bb27a43defbcbaad7fc7edd373962
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-07-12 00:08:31 +0430

    Add associations to models!

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,7 @@
 class Payment < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
